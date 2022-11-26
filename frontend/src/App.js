7ed72eba096532e106a4649684ae2c38832c9499
Tanya Zhong <tanyazhong1@gmail.com>
2022-11-25 18:16:11 -0800

    move python backend to own repo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* make a dropdown to display users  */}
+          {/* use a plotting lib to make plot */}
         </p>
         <a
           className="App-link"
