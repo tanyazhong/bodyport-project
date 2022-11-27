0dd5eede19c12f8d66200a5954c63e0b398dc5dc
Tanya Zhong <tanyazhong1@gmail.com>
2022-11-27 00:29:47 -0800

    dashboard works, update README

---
+++ README.md
@@ -0,0 +1,38 @@
+# About This Project
+This project was developed for the Bodyport takehome assessment. It displays a frontend dashboard that plots health datapoints. 
+This project uses bootstrap components and the Plotly.js react component.
+
+## Running the Project
+First, follow instructions in /backend/README.md to set up the python server. Then in the /backend directory run 
+### `python main.py`
+
+Open a new terminal and in the /frontend directory run 
+### `npm install` to install dependencies
+### `npm start`
+
+## Task 2
+Suppose new data points are added and the UI needs to be updated.
+These could be patient symptoms, hospital visits, medication changes, diet logs, risk calculations, etc...
+
+Write up a proposal for the next version of your dashboard.
+It should address these questions, as well as any other product or design concerns that come to mind:
+
+* How should these extra data points be surfaced on your site?
+* How should the API be updated to support your new design?
+* Are there any performance limitations that should be taken into consideration?
+* How would future developers extend your work?
+* Can you assess the usability of newly added features?
+
+1. For new metrics to be added to the dashboard, information needs to be added to the hardcoded 'measurements' array (line 52 of \frontend\src\App.js). This array contains an object for each metric with one property 'propName' storing the json key of the metric and another property 'name' storing the frontend display string of the metric so that the portal can display the name of the metric that is selected. In the future perhaps this information can be sent through an API to eliminate the hardcoded variable.
+
+2. As mentioned before, an API endpoint can be made that maps each metric's json key to its desired display name. Also, an API endpoint where userIDs are mapped to the patient's name would allow for the user selection dropdown to be more usable, since it would be able to display names rather than IDs. The current provided data does not include the patient name.
+
+3. With the current amount of data I did not encounter performance issues. If there were many more points there could potentially arise performance issues,
+since every time a new measurement or userID is selected the frontend goes through every point in the data to identify points that need to be graphed (that is, the points that match the current userID and measurement selections). One solution that would make this process more efficient would be to group the points by userID (using an object, probably) on initial render so that there are fewer points to process when identifying points that need to be graphed.
+
+4. It would be nice to be able to search the userID dropdown because it could become hard to use with more userIDs. As previously mentioned, it would also be good to match the userIDs to the patient's name to make the user selection dropdown to be more usable.
+
+5. I find the dashboard to be simple and intuitive, and therefore usable. As previously mentioned, one thing that would improve usability is to display the patient's name in the user selection dropdown, rather than the ID.
+
+## Other Notes
+I ran into some issues setting up the python server. First, I ran `./venv/scripts/activate.bat`  instead of `source venv/bin/activate` since I have a windows computer. I also had to change requirements.txt to upgrade Flask to 2.1.0 and Flask-SQLAlchemy to 2.5.1.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,124 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
+import Plot from 'react-plotly.js';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([{}]) // raw data
+  const [users, setUsers] = useState([]) // array of unique user ids used for userID dropdown
+  const [curUser, setCurUser] = useState(null) // store the user id of the selected user
+  const [curMeasurement, setCurMeasurement] = useState({}) // store the selected measurement
+  const [graphData, setGraphData] = useState({ x: [], y: [] }) // data gets fed to plot. x will always be date, y depends on selection
+
+  useEffect(() => {
+    fetch("/measurements").then(
+      res => res.json()
+    ).then(
+      data => {
+        let tempUsers = new Set()
+        let dataLength = Object.keys(data).length
+        for (let i = 0; i < dataLength; i++) {
+          let point = data[i]
+          tempUsers.add(point.user_id)
+        }
+        setData(data)
+        setUsers(Array.from(tempUsers))
+        setCurUser(users[0])
+      }
+    )
+  }, []) // fetches data on first render
+
+  useEffect(() => {
+    let dataLength = Object.keys(data).length
+    let tempGraphData = {
+      x: [],
+      y: []
+    }
+    for (let i = 0; i < dataLength; i++) {
+      let point = data[i]
+      // check that the point belongs to the selected user, 
+      // and there exists data for the date and the selected measurement
+      if (point.user_id === curUser && point[curMeasurement.propName] && point["creation_date"]) {
+        let date = new Date(point["creation_date"]).toISOString()
+        tempGraphData.x.push(date)
+        tempGraphData.y.push(point[curMeasurement.propName])
+      }
+    }
+    setGraphData(tempGraphData)
+  }, [curUser, curMeasurement]) // find relevant data points when curUser or curMeasurement is changed
+
+  // hardcode measurements of interest
+  let measurements = [
+    {
+      propName: "backend_sway_area_mm2",
+      name: "Sway Area"
+    },
+    {
+      propName: "body_fat_percent",
+      name: "Body Fat Percent"
+    },
+    {
+      propName: "heart_rate",
+      name: "Heart Rate"
+    },
+    {
+      propName: "peak_count",
+      name: "Peak Count"
+    },
+    {
+      propName: "weight_kg",
+      name: "Weight (kg)"
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* make a dropdown to display users  */}
-          {/* use a plotting lib to make plot */}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+      {/* dropdown selects users  */}
+      <Dropdown className='dropdown'>
+        <Dropdown.Toggle className='dropdown-toggle'> {curUser || "User ID"} </Dropdown.Toggle>
+        <Dropdown.Menu>
+          {users.map((userId, i) =>
+            <Dropdown.Item key={i} onClick={() => setCurUser(users[i])}>
+              {userId}
+            </Dropdown.Item>
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
+
+      {/* dropdown selects the measurement to graph */}
+      <Dropdown className='dropdown'>
+        <Dropdown.Toggle variant="primary"> {curMeasurement.name || "Measurement"}</Dropdown.Toggle>
+        <Dropdown.Menu>
+          {measurements.map((measurement, i) =>
+            <Dropdown.Item key={i} onClick={() => setCurMeasurement(measurement)}>
+              {measurement.name}
+            </Dropdown.Item>
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
+      </div>
+
+      {/* use a plotting lib to make plot if a measurement and user is selected*/}
+      <div className='plot-container'>
+        {curMeasurement && curUser ? (
+          <Plot
+            data={[
+              {
+                x: graphData.x,
+                y: graphData.y,
+                type: 'scatter',
+                mode: 'markers',
+                marker: { color: 'red' },
+              }
+            ]}
+            layout={{ width: 900, height: 500, title: curMeasurement.name }}
+          />) : (
+          <p> Please select a userID and a measurement to view a plot. </p>
+        )
+        }
+      </div>
     </div>
   );
 }
